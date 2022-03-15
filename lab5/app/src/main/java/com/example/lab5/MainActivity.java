1bf0e0e8f096ec6a8bba9bf221fd5966bb7f85a2
Thuan Hoang <thamthuy100272@gmail.com>
2022-03-15 15:15:53 +0700

    cap nhat hinh anh

--- lab5/app/src/main/java/com/example/lab5/MainActivity.java
+++ lab5/app/src/main/java/com/example/lab5/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -13,10 +15,14 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    EditText txtSearch;
     ListView  lvDonut;
     DonutAdapter donutAdapter ;
     ArrayList<Donut> donuts ;
+    ArrayList<Donut> ArrSearchdonut;
+    Button btnSearch;
+    EditText editextSr;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +30,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         lvDonut = findViewById(R.id.listviewdonut);
         donuts = new ArrayList<>();
+        ArrSearchdonut = new ArrayList<>();
         donuts.add( new Donut("Tasty Donut","Spicy tasty donut family","$10.00",R.drawable.donut_yellow_1));
         donuts.add( new Donut("Pink Donut","Spicy tasty donut family","$20.00",R.drawable.tasty_donut_1));
         donuts.add( new Donut("Floating Donut","Spicy tasty donut family","$30.00",R.drawable.green_donut_1));
         donuts.add( new Donut("Tasty Donut","ngon thế nhờ","$100.00",R.drawable.donut_red_1));
         donutAdapter = new DonutAdapter(this,R.layout.donut1dong,donuts);
+
         lvDonut.setAdapter(donutAdapter);
+//        btnSearch.findViewById(R.id.btnSearch);
+//        editextSr.findViewById(R.id.txtsearch);
+//
+//        btnSearch.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                ArrSearchdonut.clear();
+//                for (Donut donut:donuts) {
+//                    String s = editextSr.getText().toString();
+//                    if(donut.getName().contains(s)){
+//                        ArrSearchdonut.add(donut);
+//                    }
+//                }
+//                lvDonut.setAdapter(new DonutAdapter(MainActivity.this,R.layout.donut1dong,ArrSearchdonut));
+//            }
+//        });
    
 
 
