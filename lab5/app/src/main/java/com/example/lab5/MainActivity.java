564ad112d86d213871f4d56df81ec4001e786727
Thuan Hoang <thamthuy100272@gmail.com>
2022-03-17 00:10:16 +0700

    finish feature search

--- lab5/app/src/main/java/com/example/lab5/MainActivity.java
+++ lab5/app/src/main/java/com/example/lab5/MainActivity.java
@@ -4,13 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import android.text.Editable;
-import android.text.TextWatcher;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -19,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
     DonutAdapter donutAdapter ;
     ArrayList<Donut> donuts ;
     ArrayList<Donut> ArrSearchdonut;
-    Button btnSearch;
-    EditText editextSr;
+    ImageButton btnSearch;
+    EditText txtSearch;
 
 
 
@@ -30,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         lvDonut = findViewById(R.id.listviewdonut);
         donuts = new ArrayList<>();
-        ArrSearchdonut = new ArrayList<>();
         donuts.add( new Donut("Tasty Donut","Spicy tasty donut family","$10.00",R.drawable.donut_yellow_1));
         donuts.add( new Donut("Pink Donut","Spicy tasty donut family","$20.00",R.drawable.tasty_donut_1));
         donuts.add( new Donut("Floating Donut","Spicy tasty donut family","$30.00",R.drawable.green_donut_1));
@@ -38,23 +36,24 @@ public class MainActivity extends AppCompatActivity {
         donutAdapter = new DonutAdapter(this,R.layout.donut1dong,donuts);
 
         lvDonut.setAdapter(donutAdapter);
-//        btnSearch.findViewById(R.id.btnSearch);
-//        editextSr.findViewById(R.id.txtsearch);
-//
-//        btnSearch.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                ArrSearchdonut.clear();
-//                for (Donut donut:donuts) {
-//                    String s = editextSr.getText().toString();
-//                    if(donut.getName().contains(s)){
-//                        ArrSearchdonut.add(donut);
-//                    }
-//                }
-//                lvDonut.setAdapter(new DonutAdapter(MainActivity.this,R.layout.donut1dong,ArrSearchdonut));
-//            }
-//        });
-   
+
+        txtSearch = findViewById(R.id.txtsearch);
+        btnSearch = findViewById(R.id.btnSearch);
+        ArrSearchdonut = new ArrayList<>();
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrSearchdonut.clear();
+                for(Donut food: donuts) {
+                    String st = txtSearch.getText().toString();
+                    if(food.getName().contains(st)){
+                        ArrSearchdonut.add(food);
+                    }
+                }
+                lvDonut.setAdapter(new DonutAdapter(MainActivity.this, R.layout.donut1dong, ArrSearchdonut));
+            }
+        });
 
 
     }
